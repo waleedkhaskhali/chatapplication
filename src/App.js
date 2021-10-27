08d859a1b7f74f92c4c57412322a57e1dc5a4e6f
waleedkhaskhali <76614771+waleedkhaskhali@users.noreply.github.com>
2021-10-27 17:45:23 -0400

    loginpage

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import { ChatEngine } from 'react-chat-engine';
 
-import ChatFeed from './components/ChatFeed'
-
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 import './App.css';
 
-const App = () => {
-    return (
-        <ChatEngine
-        height="100vh"
-        projectID="cf8215d6-bd4a-44ba-ad52-001c22221460"
-        userName="Waleedk2000"
-        userSecret="123123"
-        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/>}
-        />
-    )
-}
+const projectID = 'cf8215d6-bd4a-44ba-ad52-001c22221460';
 
-export default App
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
+// infinite scroll, logout, more customizations...
+
+export default App;
